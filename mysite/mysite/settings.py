8a9e9021467e2f4f7d3a32f3fc732eb40f4d8d0c
seela-2001 <sohilamustafa68@gmail.com>
2024-09-17 14:12:09 +0300

    secure email

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -130,8 +130,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import os
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sohila.mustafa40@gmail.com'
-EMAIL_HOST_PASSWORD = '12345678'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
